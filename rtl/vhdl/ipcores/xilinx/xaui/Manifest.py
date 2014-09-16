1a706522de1a6b9a16cf520ff4c6e27df109c9f0
Michele <michele.quinto@cern.ch>
2014-09-16 12:51:57 +0200

    Adding Manifest.py files to generate similation Makefiles http://www.ohwr.org/projects/hdl-make

---
+++ rtl/vhdl/ipcores/xilinx/xaui/Manifest.py
@@ -0,0 +1,8 @@
+files = [ "./xaui_v10_4.vhd",
+          "./xaui_v10_4/simulation/demo_tb.vhd",
+          "./xaui_v10_4/example_design/xaui_v10_4_gtx_wrapper_gtx.vhd",
+          "./xaui_v10_4/example_design/xaui_v10_4_example_design.vhd"
+          "./xaui_v10_4/example_design/xaui_v10_4_tx_sync.vhd",
+          "./xaui_v10_4/example_design/xaui_v10_4_gtx_wrapper.vhd",
+          "./xaui_v10_4/example_design/xaui_v10_4_block.vhd",
+          "./xaui_v10_4/example_design/xaui_v10_4_chanbond_monitor.vhd" ]
